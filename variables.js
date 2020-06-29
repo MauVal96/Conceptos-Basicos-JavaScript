24637eb01d3ea58a7cbbd3fa9e3779f96f1c38ac
MauVal96 <m.valadezolmos@ugto.mx>
2020-06-29 14:06:25 -0500

    Variables II

---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Pedro";
+
+// Declarar 
+var edad;
+
+edad
+// Incializar 
+edad = 30;
+
+edad
+
+var elementos = ["Computadora ", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+};
+
+persona
